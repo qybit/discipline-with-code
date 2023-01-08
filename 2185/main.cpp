b9390b1d8558f1179cfdc680b7f334907f898f32
qybit <2601970213@qq.com>
2023-01-08 19:02:58 +0800

    -fix:移除vscode配置项

---
+++ 2185/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int prefixCount(vector<string> &words, string pref)
+    {
+        cout << 1 << endl;
+        return 0;
+    }
+};
+
+void solve()
+{
+    Solution *s = new Solution();
+    cout << 1 << endl;
+}
+int main()
+{
+    solve();
+    return 0;
+}
